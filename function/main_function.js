29b1595b989237acc5ebed22427f352baa0a61b8
ParkWonYeop <wyp9679@gmail.com>
2022-08-06 03:01:57 +0900

    first commit

---
+++ function/main_function.js
@@ -0,0 +1,28 @@
+const { request_api,user_database,set_date,set_time,check_api } = require("./api_functionjs");
+const { set_app } = require("./web_function");
+const express = require('express');
+const { createServer } = require('http');
+const router = require('../router/app.js');
+
+//서버를 실행시킴//
+const run_server = function(){
+    const app = express();
+    set_app(app,router);
+    const server = createServer(app).listen(80);
+}
+
+//API에 데이터를 지속적으로 요청함//
+const run_api = function(connection){
+    const connection = user_database();
+    let base_date = set_date();
+    let base_time = set_time();
+    request_api(connection,base_date,base_time);
+    setInterval(function(){
+        base_time = check_api(connection,base_time)
+    }, 600000)
+}
+
+module.exports = {
+    run_server:run_server,
+    run_api:run_api
+}
